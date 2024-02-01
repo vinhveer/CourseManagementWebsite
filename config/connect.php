c64551d47d4002125e34536dadf3e39e155ddfef
Hoc de biet <153816852+NguyenMtai123@users.noreply.github.com>
2024-02-01 16:08:23 +0700

    Add files via upload

---
+++ config/connect.php
@@ -0,0 +1,11 @@
+<?php
+    //cau hinh ket noi
+    $db_localhost="localhost";
+    $db_user="root";
+    $db_pass="";
+    $db_data="course_management";
+    $cookie_name = 'aba';
+    $cookie_time = (3600 * 24 * 30); // 30 days
+    $dbconnect=mysqli_connect($db_localhost,$db_user,$db_pass,$db_data) or die('ket noi khong thanh cong');
+    mysqli_query($dbconnect,"set names 'utf8'");
+?>
