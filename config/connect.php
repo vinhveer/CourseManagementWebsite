e4dd1cf90f92d59ad203b82edaa9b2da46f10a52
Hoc de biet <153816852+NguyenMtai123@users.noreply.github.com>
2024-02-01 18:55:51 +0700

    Update connect.php

--- config/connect.php
+++ config/connect.php
@@ -3,9 +3,13 @@
     $db_localhost="localhost";
     $db_user="root";
     $db_pass="";
-    $db_data="course_management";
-    $cookie_name = '';
+    $db_data="course_management1";
+    $cookie_name = 'abc';
     $cookie_time = (3600 * 24 * 30); // 30 days
-    $dbconnect=mysqli_connect($db_localhost,$db_user,$db_pass,$db_data) or die('ket noi khong thanh cong');
-    mysqli_query($dbconnect,"set names 'utf8'");
+    $dbconnect=mysqli_connect($db_localhost,$db_user,$db_pass,$db_data);
+    if($dbconnect){
+        mysqli_query($dbconnect, "SET NAMES 'utf8'");
+    }else{
+        echo "Kết nối thất bại";
+    }
 ?>
