cc0ac4f7b489a086ca02ac2533f6b04a2ffbe415
Hoc de biet <153816852+NguyenMtai123@users.noreply.github.com>
2024-02-07 08:18:45 +0700

    Delete logout.php

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    if(isset($_SESSION['mySession'])){
-        unset($_SESSION['mySession']);
-    }
-    setcookie("abc", "", time() -3600);
-        header('location: login.php');
-?>
